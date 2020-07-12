af758d008d9b68407c77fcdaac2dfd9774e776f7
TMurteira <tiagomurteira2000@gmail.com>
2020-07-12 16:32:34 +0100

    added content to meme.js

--- src/services/meme.js
+++ src/services/meme.js
@@ -1,5 +1,10 @@
-//import { apiRequest } from "../configs/apiMiddleware";
+import { apiRequest } from "../configs/apiMiddleware";
 
 export default {
-  
-}
+  getAll: (searchText) => apiRequest("GET", `/meme`, { query: { search: searchText } }),
+  getOne: (id) => apiRequest("GET", `/meme/${id}`),
+  create: (jsonData) => apiRequest("POST", `/meme`, { jsonData }),
+  update: (id, jsonData) => apiRequest("PUT", `/meme/data/${id}`, { jsonData }),
+  //setCover: (id, formData) => apiRequest("PUT", `/book/cover/${id}`, { formData }),
+  remove: (id) => apiRequest("DELETE", `/meme/${id}`),
+};
